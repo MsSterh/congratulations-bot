2d1cabc8d759275698259c7adb45fc3204d23fef
Anastasiia Bakuta <ae@anadeainc.com>
2021-04-11 11:09:47 +0300

    Update code

--- bot.js
+++ bot.js
@@ -6,18 +6,23 @@ let bot;
 if(process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
   bot.setWebHook(process.env.HEROKU_URL + bot.token);
-}
-else {
+} else {
   bot = new Bot(token, { polling: true });
 }
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
+function getRandomCongrats(name) {
+  return 'С Днем Рождения, ' + name + '!';
+}
+
 bot.on('message', (msg) => {
-  const name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(() => {
-    // reply sent!
-  });
+  if (msg.text === '') {
+    bot.sendMessage(msg.chat.id, 'Введите имя поздравителя');
+    return;
+  }
+
+  bot.sendMessage(msg.chat.id, getRandomCongrats(msg.text), {parse_mode: 'HTML'});
 });
 
 module.exports = bot;
