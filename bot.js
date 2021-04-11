7e52814710a78c11f105a41fb6771066220c5108
Anastasiia Bakuta <ae@anadeainc.com>
2021-04-11 13:14:42 +0300

    Add firebase

--- bot.js
+++ bot.js
@@ -3,7 +3,32 @@ const token = process.env.TOKEN;
 const Bot = require('node-telegram-bot-api');
 let bot;
 
-if(process.env.NODE_ENV === 'production') {
+const admin = require('firebase-admin');
+
+let serviceAccount = require('./serviceAccountKey.json');
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.GOOGLE_FIREBASE_URL
+  });
+}
+
+const writeData = (text) => {
+  admin.database().ref('items').push().set(text);
+};
+
+const getRandomData = (id) => {
+  let ref = admin.database().ref('items');
+  ref.once('value', function (snapshot) {
+    const items = Object.values(snapshot.val());
+    const randomCongrat = items[Math.floor(Math.random() * items.length)];
+    bot.sendMessage(id, randomCongrat, {parse_mode: 'HTML'});
+  });
+};
+
+
+if (process.env.NODE_ENV === 'production') {
   bot = new Bot(token);
   bot.setWebHook(process.env.HEROKU_URL + bot.token);
 } else {
@@ -12,17 +37,29 @@ if(process.env.NODE_ENV === 'production') {
 
 console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
-function getRandomCongrats(name) {
-  return 'С Днем Рождения, ' + name + '!';
-}
+const defaultMessage = '/list - список доступных команд\n' +
+'/add - добавление нового поздравления в список, через пробел\n' +
+'/get - получение рандомного поздравления\n';
 
 bot.on('message', (msg) => {
-  if (msg.text === '') {
-    bot.sendMessage(msg.chat.id, 'Введите имя поздравителя');
+  if (msg.text.startsWith('/start') || msg.text.startsWith('/list')) {
+    bot.sendMessage(msg.chat.id, defaultMessage, {parse_mode: 'HTML'});
     return;
   }
 
-  bot.sendMessage(msg.chat.id, getRandomCongrats(msg.text), {parse_mode: 'HTML'});
+  if (msg.text.startsWith('/add ')) {
+    writeData(msg.text.slice(5));
+    msg.text.slice(5);
+    bot.sendMessage(msg.chat.id, 'Новое поздравление успешно добавлено в список!');
+    return;
+  }
+
+  if (msg.text.startsWith('/get')) {
+    getRandomData(msg.chat.id);
+    return;
+  }
+
+  bot.sendMessage(msg.chat.id, defaultMessage, {parse_mode: 'HTML'});
 });
 
 module.exports = bot;
